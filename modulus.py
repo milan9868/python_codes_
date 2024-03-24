47937ea58f28b6a31b6b12e0e1e874fc237a13a3
Milan Stojanovic <milanstojanovic998@gmail.com>
2024-03-24 13:39:45 +0100

    bla0

--- modulus.py
+++ modulus.py
@@ -2,4 +2,7 @@ a = int(input("Enter The Dividen\n"))
 b = int(input("Enter The Divisor\n"))
 r = b%a
 
-print(f"when {a} is divided by {b}, the remainder is {r}")  
+print(f"when {a} is divided by {b}, the remainder is {r}")
+
+
+print("New change")
